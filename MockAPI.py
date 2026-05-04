8c0e37fb532fe53caadfcaa315c8bcc8249dd086
Gauri Kute <gaurikute@Gauris-MacBook-Air.local>
2026-05-04 21:23:35 +0530

    Initial Structure

---
+++ MockAPI.py
@@ -0,0 +1,17 @@
+from flask import Flask, request,jsonify
+import json
+
+app = Flask(_name_)
+
+mock_method = None
+mock_response = None
+
+def get_user_input():
+    global mock_method, mock_response
+    print("====== MOCK API GENERATOR ======")
+    
+    # Input for method
+    method = input("Enter HTTP method to mock (GET/POST/PUT/DELETE): ").upper()
+
+
+mock_method = method
